812bcda5476c0342fbc7a2cdac406a9479a2234f
salma-4 <salmasobhy456@gmail.com>
2024-05-13 23:03:15 +0200

    up

--- FileTransimision/Client.java
+++ FileTransimision/Client.java
@@ -3,18 +3,20 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        final String Host = "127.0.0.1";
+        final String HOST = "127.0.0.1";
         final int PORT_NUMBER = 8081;
-
-        try (Socket socket = new Socket(Host, PORT_NUMBER);
+        try (Socket socket = new Socket(HOST, PORT_NUMBER);
              InputStream inputStream = socket.getInputStream();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+             FileWriter writer = new FileWriter("outputFile.txt")) {
 
             String readLine;
             while ((readLine = reader.readLine()) != null) {
+                writer.write(readLine + "\n"); // Write each line to the file
                 System.out.println(readLine);
+
             }
-            System.out.println("File received successfully!");
+            System.out.println("File received and saved successfully!");
         } catch (IOException e) {
             System.err.println("Error: Failed to establish connection or read data from the server.");
             e.printStackTrace();
